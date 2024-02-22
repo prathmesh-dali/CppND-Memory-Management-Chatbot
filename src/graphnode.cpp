b504af02b61ade24a979c546a6b2c35ea37f4d17
Prathmesh Dali <prathmesh.dali@gmail.com>
2024-02-23 03:22:43 +0530

    refactor

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -1,46 +1,33 @@
-#include "graphedge.h"
 #include "graphnode.h"
 
-GraphNode::GraphNode(int id)
-{
-    _id = id;
+#include "graphedge.h"
+
+GraphNode::GraphNode(int id) { _id = id; }
+
+GraphNode::~GraphNode() {
+  // delete _chatBot;
 }
 
-GraphNode::~GraphNode()
-{
-    // delete _chatBot; 
+void GraphNode::AddToken(std::string token) { _answers.push_back(token); }
 
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge) {
+  _parentEdges.push_back(edge);
 }
 
-void GraphNode::AddToken(std::string token)
-{
-    _answers.push_back(token);
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge) {
+  _childEdges.push_back(std::move(edge));
 }
 
-void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
-{
-    _parentEdges.push_back(edge);
+void GraphNode::MoveChatbotHere(ChatBot chatbot) {
+  _chatBot = std::move(chatbot);
+  _chatBot.SetCurrentNode(this);
 }
 
-void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
-{
-    _childEdges.push_back(std::move(edge));
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode) {
+  newNode->MoveChatbotHere(std::move(_chatBot));
+  // _chatBot = nullptr; // invalidate pointer at source
 }
 
-void GraphNode::MoveChatbotHere(ChatBot chatbot)
-{
-    _chatBot = std::move(chatbot);
-    _chatBot.SetCurrentNode(this);
-}
-
-void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
-{
-    newNode->MoveChatbotHere(std::move(_chatBot));
-    // _chatBot = nullptr; // invalidate pointer at source
-}
-
-GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
-{
-    return _childEdges[index].get();
-
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index) {
+  return _childEdges[index].get();
 }
